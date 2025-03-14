5bdbb487111411a5c2451d40a645f59d0c14a961
JoeTech89 <arjoe89@gmail.com>
2025-03-14 18:05:33 -0400

    Add files via upload

---
+++ JoeTech89.github.io-main/static/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert(Javascript Running)
+    // const target_main = document.querySelectorAll('.main');
+    // const target_topmenu = document.querySelector('.topmenu');
+    // const target_infobar = document.querySelector('.infobar');
+    // const target_content = document.querySelector('.content');
+    // const target_sidemenu = document.querySelector('.sidemenu')
+    // const barheight = target_topmenu.offsetHeight + target_infobar.offsetHeight;
+    // target_content.setAttribute('style','height:calc(100vh - '+barheight+'px)');
+    // target_sidemenu.setAttribute('style','height:calc(100vh - '+barheight+'px)');
+})
